014eb1f5a1f62ab042ffdac0e7f349e8a0c761dc
Your Name <natoli.missy@gmail.com>
2021-04-04 09:38:53 -0500

    starting code for password criteria variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,31 @@
 // Assignment code here
+function genCriteria() {
+  //password length
+  var passwordLength = window.prompt("How many characters would you like your password? Please enter a number between 8 and 128."); {
+    
+  if (passwordLength === "" || passwordLength === null || passwordLength <8 || passwordLength > 128) {
+    window.alert("Please enter a number between 8 and 128.");
+    return genCriteria();
+  }
+  else {
+    window.confirm("Your password will be " + passwordLength + " characters long.");
+    break;
+  }
+};
+
+    //lowercase/uppercase
+    var lowercase = window.prompt("Would you like to include both lowercase and uppercase letters? Please enter 'yes' or 'no'."); {
+    lowercase = lowercase.toLowerCase
+    if (lowercase === "yes") {
+      window.alert("Your password will contain both lowercase and uppercase letters.");
+    }
+    else (lowercase === "no") {
+      window.prompt("Would you like only lowercase letters? Please enter 'yes' or 'no'.");
+      break;
+    }
+  }
+}
+};
 
 
 // Get references to the #generate element
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Password Generator Starter Code
+Create variables containing different data types used to create a password
+
+Write conditional statements to add in password criteria that the user has selected
+
+Write for loops to create a password from the variables you created
+
+Use functions to display the password to the user
+
+Use GitHub Pages to publish the page to the web
